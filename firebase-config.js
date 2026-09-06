d4b5eec1b2744912e5b9049cdb40438d29c98099
toolapss-MWG <whimasnanjar4@gmail.com>
2026-09-07 00:31:47 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBntT312d0m0VFSPkqiDVUomflUWzcKVB4",
-  authDomain: "ayobangun-contractor.firebaseapp.com",
-  projectId: "ayobangun-contractor",
-  storageBucket: "ayobangun-contractor.firebasestorage.app",
-  messagingSenderId: "1099399042051",
-  appId: "1:1099399042051:web:764118e26a6cc479956a36",
-  measurementId: "G-01F0KBG41D"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+{
+"hosting":{"public":".","ignore":["firebase.json"]}
+}
